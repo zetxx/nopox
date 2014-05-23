3c25558f45b86ae06e1bc609a10a649d141d4f6e
zetxx <zetxx@belogradchik.biz>
2014-05-23 18:26:33 +0300

    refactory

--- test/main.js
+++ test/main.js
@@ -1,1 +1,13 @@
-{}
+var nopox = require('./lib/main.js')({"remoteHost":'localhost',"remotePort":80,"listenPort":8384});
+
+var Nopox = nopox.create();
+Nopox.listen();
+Nopox.on('request',function(data){
+  var _str = data.toString();
+  _str=_str.replace(/Host\:[^\n\r]+/ig,'Host: kalin');
+  return new Buffer(_str);
+});
+
+Nopox.on('response',function(data,done){
+    done(data);
+});
