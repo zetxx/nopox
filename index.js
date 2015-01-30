7800903583e8e48a12d517a1aae36559f088a827
Elin Ahmedow <zetxx@belogradchik.biz>
2015-01-30 18:00:43 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,121 +1,55 @@
-var net = require('net'),
-    util = require('util');
+var net = require('net');
 
-function nopox(remoteHost,remotePort,listenPort){
-  this.remoteHost = remoteHost;
-  this.remotePort = remotePort;
-  this.listenPort = listenPort;
-  this.onConnectionEvent = {
-    "request":function(data){console.log('req: %s',data.toString());return data;}
-    ,"response":function(data,done){console.log('resp: %s',data.toString());done(data);}
-  };
-  var self = this;
+function _remote(host, port, localConnection, onReq, onResp){
+	onReq = onReq || function(done, data){done(data)};
+	onResp = onResp || function(done, data){done(data)};
 
-  try {
-    this.server = net.createServer(function(s){self.onBind(s);});
-  } catch(e){
-    console.log('Error while binding local port.', e);
-  }
-
-  this.server.on('error',function(error){
-    console.log('"serv" conn error');
-    console.log(error);
-  });
+	var remote = net.connect({'host':host,'port': port},function() {
+			console.log('connected to server: %s:%s', host, port);
+			localConnection.on('end', function() {
+				console.log('local client disconnected: %s', port);
+				remote.end();
+			});
+			localConnection.on('data', function(data) {
+				console.log('local client requested: %s', port);
+				var fn = function(_data){remote.write(_data);};
+				onReq(fn, data);
+			});
+		}
+	);
+	remote.on('error', function (e) {
+		console.log('error connecting to server: %s:%s', host, port);
+	});
+	remote.on('end', function (e) {
+		console.log('end connection to server: %s:%s', host, port);
+	});
+	remote.on('data', function (data) {
+		console.log('remote data received: %s:%s', host, port);
+		var fn = function(_data){localConnection.write(_data);};
+		onResp(fn, data);
+	});
 };
 
-nopox.prototype.onBind = function(servConn){
-  var self = this;
-  console.log('client conected');
-
-  try {
-      var dest = net.connect({
-        "host":this.remoteHost
-        ,"port":this.remotePort
-      },function(){self.cOnConnect();});
-  } catch(e) {
-    console.log('Error while connecting on remote side.',e);
-  }
-
-  dest.on('data',function(data){
-    self.cOnData(servConn,data);
-  });
-
-  dest.on('error', function(error){
-    console.log('"dest" conn error');
-    console.log(error);
-  });
-
-  servConn.on('end',function(){
-    self.sOnEnd(dest);
-  });
-  servConn.on('data',function(data){
-    self.sOnData(dest,data);
-  });
-};
-
-nopox.prototype.sOnData = function(dest,data){
-  this.onConnectionEvent.request(data,function(_data){
-    try{
-        dest.write(_data);
-    } catch (e){
-        util.log('Cannot write to "dest" socket');
-        util.log(e);
-    }
-  });
-};
-
-nopox.prototype.sOnEnd = function(dest){
-  console.log('client disconnected from proxy');
-  try{
-    dest.end();
-  }catch(e){
-    console.log('client disconnected from proxy ERROR:',e);
-  }
-};
-
-nopox.prototype.cOnData = function(servConn,data){
-  this.onConnectionEvent.response(data,function(_data){
-  try{
-    servConn.write(_data);
-  } catch(e){
-    util.log('Cannot write to "servConn" socket');
-    util.log(e);
-  }
-  });
-};
-
-nopox.prototype.cOnConnect = function(){
-  console.log('Conection to %s:%s successfully.',this.remoteHost,this.remotePort);
-};
-
-nopox.prototype.listen = function(){
-  try{
-    if(!this.listenPort)
-      throw "Listen port should be defined";
-    console.log('*******************');
-    console.log('Listen on port:', this.listenPort);
-    console.log('*******************');
-    this.server.listen(this.listenPort);
-  } catch (e){
-    console.log(e);
-  }
-};
-
-nopox.prototype.on = function(type,fn){
-  switch (type){
-      case 'request':
-        this.onConnectionEvent[type] = fn;
-      break;
-      case 'response':
-        this.onConnectionEvent[type] = fn;
-      break;
-  }
+function _local(port, clientConnected, host){
+	var server = net.createServer(function(c) {
+		console.log('new client connection on port: %s', port);
+		c.on('error', function() {
+			console.log('client connection error %s', port);
+		});
+		clientConnected(c);
+	});
+	
+	server.listen(port, function() {
+		console.log('waiting for connection on %s:%s', host || 'localhost', port);
+	});
 };
 
 module.exports = function(settings){
   return {
     "create":function(){
-      return (new nopox(settings.remoteHost,settings.remotePort,settings.listenPort));
+		new _local(settings.listenPort, function(localConnection){
+			new _remote(settings.remoteHost, settings.remotePort, localConnection, settings.onReq, settings.onResp);
+		});
     }
   };
 };
